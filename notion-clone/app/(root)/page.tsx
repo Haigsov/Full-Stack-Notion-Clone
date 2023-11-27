586f399533de3942bb5caf03263df06e6fd83102
Haigsov <haigsouv@my.yorku.ca>
2023-11-26 23:54:35 -0500

    Removed and put it in a (root) folder

--- notion-clone/app/(root)/page.tsx
+++ notion-clone/app/(root)/page.tsx
@@ -1,6 +1,6 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
-export default function Home() { 
+export default function Home() {
   return (
     <div>
       <Button className="bg-emerald-500 text-white">
