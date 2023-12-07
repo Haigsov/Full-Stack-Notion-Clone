36de1956e5b7c6d1e057a4d2679df00284c76e53
Haigsov <haigsouv@my.yorku.ca>
2023-12-06 22:21:36 -0500

    Removed an unnecessary space

--- notion-clone/app/(Marketing)/page.tsx
+++ notion-clone/app/(Marketing)/page.tsx
@@ -3,7 +3,7 @@ import { Heading } from "./_components/heading";
 const MaketingPage = () => {
   return (
     <div className="min-h-full flex flex-col">
-      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-6" >
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-6">
         <Heading />
       </div>
     </div>
